3cc1c7523871bffaa1b6a2459fe689c320f58fe0
Israel Molestina <molestinaisrael@gmail.com>
2021-03-16 16:49:54 -0400

    created inquirer function to ask what user would like to do and to display all departments

--- employee-schema.sql
+++ employee-schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 
 CREATE TABLE role (
 	role_id INT NOT NULL AUTO_INCREMENT,
-    tilte VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
 	salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (role_id),
@@ -27,3 +27,22 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role (role_id),
     FOREIGN KEY (manager_id) REFERENCES employee (employee_id) 
 );
+
+USE trackerDB;
+--Creating departments
+INSERT INTO department (name)
+VALUES ("Human Resources"), ("Development"), ("Sales");
+
+
+--Creating roles for departments
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 100000, 1), ("HR REPRESENTATIVE", 80000, 1), 
+("Dev", 90000, 2), ("Lead Developer", 120000, 2), 
+("Lead Sales Rep", 100000, 3), ("Sales Rep", 80000, 3);
+
+
+--Creating employees
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Titus", "Molestina", 1), ("Grace", "Chamberlain", 2),
+ ("Israel", "Molestina", 4), ("Sarah", "Chamberlain", 3), 
+ ("Ian", "Hopkins", 5), ("Jaseth", "Fike", 6);
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+const Choice = require('inquirer/lib/objects/choice');
+
+// console.table([
+//   {
+//     name: 'foo',
+//     age: 10
+//   }, {
+//     name: 'bar',
+//     age: 20
+//   }
+// ]);
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -19,3 +31,42 @@ connection.connect((err) => {
   if (err) throw err;
   runSearch();
 });
+
+const runSearch = () => {
+  inquirer
+  .prompt({
+    name: 'action',
+    type: 'list',
+    message: "What would you like to do?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      
+    ],
+  })
+  .then((userChoice) => {
+    switch (userChoice.action) {
+      case "View all departments":
+        showDepts();
+        break;
+
+      case "View all roles":
+        showRoles();
+        break;
+
+      case "View all employees":
+          showEmployees();
+          break;
+    }
+  });
+};
+
+const showDepts = () => {
+  const query =
+    'SELECT * FROM department';
+  connection.query(query, (err, res) => {
+    console.table(res);
+    runSearch();
+  });
+};
